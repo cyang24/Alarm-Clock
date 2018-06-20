32b6b7d63db08e025f811d5c373ea23635836e6b
Chouyou Yang <chouyouyang@Chouyous-MacBook-Pro.local>
2018-06-20 07:17:31 -0500

    Alarm Clock Functional

---
+++ AlarmClock/ClockRadio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace AlarmClock
+{
+    public class ClockRadio
+    {   
+        //variables
+
+        string time;
+        string radioStation;
+        string alarmTime;
+        bool alarmOn;
+
+
+        //constructor
+
+        public ClockRadio()
+        {
+            time = "12:00 AM";
+            radioStation = "COUNTRY";
+            alarmOn = false;
+        }
+
+
+        //functions set values
+
+        public void SetTime(string time)
+        {
+            this.time = time;
+        }
+
+        public void SetRadioStation(string radioStation)
+        {
+            this.radioStation = radioStation; 
+        }
+
+        public void SetAlarmTime(string alarmTime)
+        {
+            this.alarmTime = alarmTime;
+            alarmOn = true;
+        }
+
+        //functions display value
+
+        public void DisplayTime()
+        {
+            Console.WriteLine("the current time is " + time);
+        }
+
+        public void DisplayRadioStation()
+        {
+            Console.WriteLine("now playing " + radioStation);
+        }
+
+        public void DisplayAlarmTime()
+        {
+            Console.WriteLine("the alarm is set at " + alarmTime + " the alarm is on " + alarmOn);
+
+        }
+    }
+}
---
+++ AlarmClock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AlarmClock
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ClockRadio clock1 = new ClockRadio();
+            clock1.SetTime("5:00PM");
+            clock1.DisplayTime();
+            clock1.SetAlarmTime("6:00AM");
+            clock1.DisplayAlarmTime();
+
+            ClockRadio clock2 = new ClockRadio();
+            clock2.SetTime("12:00PM");
+            clock2.DisplayTime();
+            clock2.SetAlarmTime("5:30");
+            clock2.DisplayAlarmTime();
+
+
+
+        }
+    }
+}
